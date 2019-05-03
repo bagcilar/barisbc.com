48ad6aa6526e79178b4ee39c0552bd035aa8df1f
barisbc <bbagcilar@gmail.com>
2019-05-02 23:09:50 -0400

    build folder created

---
+++ build/precache-manifest.7aea2cde3bdacc30ae36ac011472517e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d495d3a3c569dd5f4ad3",
+    "url": "/static/js/main.d495d3a3.chunk.js"
+  },
+  {
+    "revision": "86a30d86157a0b516289",
+    "url": "/static/js/2.86a30d86.chunk.js"
+  },
+  {
+    "revision": "d495d3a3c569dd5f4ad3",
+    "url": "/static/css/main.e9acfd74.chunk.css"
+  },
+  {
+    "revision": "66c2c5f4cc372cd5eb0ea407c4ebe8bd",
+    "url": "/index.html"
+  }
+];
