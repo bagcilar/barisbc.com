299a7361e88572bc0c1bb98c1bb583219e201ebd
barisbc <bbagcilar@gmail.com>
2019-09-23 22:39:06 -0400

    error page updated

--- src/client/components/error.js
+++ src/client/components/error.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './css/error.css';
+import errorPicture from '../res/error.png'
 
 class error extends Component {
   render() {
     return (
 
-      <div className="error">
-        <p>oops! page not found...</p>
+      <div className="errorSection">
+
+        <img className="errorPicture" src={errorPicture} />
+
+        <div className="errorMessage">
+          <p>oops! page not found...</p>
+        </div>
+
       </div>
 
     );
